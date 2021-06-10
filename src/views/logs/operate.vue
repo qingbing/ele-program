05b8d7228fade50ad9873f902809aabddcbdb9f5
qingbing <780042175@qq.com>
2021-06-10 12:51:06 +0800

    日志管理

--- src/views/logs/operate.vue
+++ src/views/logs/operate.vue
@@ -1,13 +1,8 @@
 <script>
 // 导入包
 import EListTable from "@/extends/list-table.vue";
-import {
-  getHeaderOptions,
-  getFormOptions,
-  getOptionSystems,
-  getOptionRouteTypes,
-} from "@/api/pub";
-import { isEmpty, merge, copy, asyncAll } from "@qingbing/helper";
+import { getHeaderOptions, getFormOptions, getOptionSystems } from "@/api/pub";
+import { merge, copy, asyncAll } from "@qingbing/helper";
 import Labels from "@/conf/labels";
 import ReqLogs from "@/api/logs";
 
@@ -23,11 +18,9 @@ export default {
       query: {
         search: {
           system_alias: "",
-          route_type: "",
-          is_operate: "",
-          is_success: "",
-          route: "",
+          id: "",
           trace_id: "",
+          type: "",
           keyword: "",
           ip: "",
           uid: "",
@@ -38,42 +31,21 @@ export default {
             input_type: "input-select",
             label: "所属系统",
             exts: {
-              changeCallback: this.querySystemChange,
               clearable: true,
               options: {},
             },
           },
-          route_type: {
-            input_type: "input-select",
-            label: "路由类型",
-            exts: {
-              clearable: true,
-              options: {},
-            },
-          },
-          is_operate: {
-            input_type: "input-select",
-            label: "操作类",
-            exts: {
-              clearable: true,
-              options: Labels.yesNo,
-            },
-          },
-          is_success: {
-            input_type: "input-select",
-            label: "是否成功",
-            exts: {
-              clearable: true,
-              options: Labels.yesNo,
-            },
-          },
-          route: {
+          id: {
             input_type: "input-text",
-            label: "路由",
+            label: "日志ID",
           },
           trace_id: {
             input_type: "input-text",
             label: "Trace-ID",
+          },
+          type: {
+            input_type: "input-text",
+            label: "操作类型",
           },
           keyword: {
             input_type: "input-text",
@@ -102,44 +74,24 @@ export default {
         ],
       },
       viewDailog: {
-        title: "查看路由访问日志",
+        title: "查看操作日志",
         buttons: ["cancel"],
         defaultEntity: {},
       },
     };
   },
   methods: {
-    beforeRender(item, idx) {
-      item.input = JSON.parse(item.input);
-      item.output = JSON.parse(item.output);
-      item.exts = JSON.parse(item.exts);
-    },
-    // 改变筛选系统时，级联改变路由类型
-    querySystemChange() {
-      this.query.search.route_type = "";
-      if (isEmpty(this.query.search.system_alias)) {
-        this.query.searchItems.route_type.exts.options = {};
-      } else {
-        getOptionRouteTypes(this.query.search.system_alias)
-          .then((res) => {
-            this.query.searchItems.route_type.exts.options = res.data;
-          })
-          .catch((err) => err);
-      }
-    },
     getHeaders(cb) {
       // 获取item，系统
       const promise = {
         systems: getOptionSystems(),
-        options: getFormOptions("program-logs-route"),
-        routeTypes: getOptionRouteTypes(),
-        headers: getHeaderOptions("program-logs-route"),
+        options: getFormOptions("program-logs-operate"),
+        headers: getHeaderOptions("program-logs-operate"),
       };
       asyncAll(promise, (res) => {
         const headers = res.headers;
         // 系统
         headers.system_alias.options = res.systems;
-        headers.route_type.options = res.routeTypes;
         // 列表操作
         headers.operate.params = {
           buttons: [{ operType: "view", handle: this.buttonView }],
@@ -151,9 +103,6 @@ export default {
         const items = res.options;
         items.system_alias.exts = {
           options: res.systems,
-        };
-        items.route_type.exts = {
-          options: res.routeTypes,
         };
         this.viewDailog.items = copy(items);
       });
